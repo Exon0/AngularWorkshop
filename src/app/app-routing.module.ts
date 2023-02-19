526bae4121b96b18cb102ec3ccd4543838f559d6
Exon0 <ilyes.bettaieb@esprit.tn>
2023-02-19 13:54:20 +0100

    Add HomePage

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,24 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AddArticleComponent } from './add-article/add-article.component';
+import { ArticlesComponentComponent } from './articles-component/articles-component.component';
+import { HomeComponent } from './home/home.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { OffresEmploiComponentComponent } from './offres-emploi-component/offres-emploi-component.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"home",component:HomeComponent},
+
+  {path:"article",component:ArticlesComponentComponent},
+  {path:"offre",component:OffresEmploiComponentComponent},
+  {path:"addArticle",component:AddArticleComponent},
+
+
+  {path:"**",component:NotFoundComponent},
+
+
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,22 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { OffresEmploiComponentComponent } from './offres-emploi-component/offres-emploi-component.component';
 import { ArticlesComponentComponent } from './articles-component/articles-component.component';
+import { TemplateDrivenFormComponentComponent } from './template-driven-form-component/template-driven-form-component.component';
+import { HeaderComponent } from './header/header.component';
+import { HomeComponent } from './home/home.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { AddArticleComponent } from './add-article/add-article.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     OffresEmploiComponentComponent,
-    ArticlesComponentComponent
+    ArticlesComponentComponent,
+    TemplateDrivenFormComponentComponent,
+    HeaderComponent,
+    HomeComponent,
+    NotFoundComponent,
+    AddArticleComponent
   ],
   imports: [
     BrowserModule,
